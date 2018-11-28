9c03e00c1013d73a0e78a82b0686ab3a4e02ea5e
Doensel <dan.weissen@gmail.com>
2018-11-28 17:46:33 +0100

    Add AsyncActivity
    Query Utils not finished yet

---
+++ app/src/main/java/com/example/android/openmensa/MensaLoader.java
@@ -0,0 +1,47 @@
+package com.example.android.openmensa;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import com.example.android.openmensa.ExpandableRListView.Mensa;
+
+import java.util.List;
+
+public class MensaLoader extends AsyncTaskLoader<List<Mensa>> {
+
+    /** Tag for log messages */
+    private static final String LOG_TAG = MensaLoader.class.getName();
+
+    /** Query URL */
+    private String mUrl;
+
+    /**
+     * Constructs a new {@link EarthquakeLoader}.
+     *
+     * @param context of the activity
+     * @param url to load data from
+     */
+    public MensaLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    /**
+     * This is on a background thread.
+     */
+    @Override
+    public List<Mensa> loadInBackground() {
+        if (mUrl == null) {
+            return null;
+        }
+
+        // Perform the network request, parse the response, and extract a list of earthquakes.
+        List<Mensa> earthquakes = QueryUtils.fetchEarthquakeData(mUrl);
+        return earthquakes;
+    }
+}
